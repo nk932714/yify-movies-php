de0d84b9437e9fdab2644e53c0339bfe420d5d76
nk <nk932714@gmail.com>
2018-08-15 18:37:33 +0530

    fixed many things

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
  <head>
        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
        <title>Yify Torrents</title>
-       <link rel="stylesheet" type="text/css" href="style.css" />
+       <link rel="stylesheet" type="text/css" href="style.css" /> <!--to make language check inline --><style>div.inline { float:right; }.clearBoth { clear:both; }</style>
        <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js"></script>
        <script type="text/javascript" src="script1.js"></script>
  </head>
@@ -34,8 +34,8 @@
                  	      $page = 0,                                   // $page Used to see the next page of movies, eg limit=15 and page=2 will show you movies 15-30
 	                      $query_term = 0,                             // $query_term Used for movie search, matching on: Movie Title/IMDb Code, Actor Name/IMDb Code, Director Name/IMDb Code
                  	      $minimum_rating = 0,                         // $minimum_rating Used to filter movie by a given minimum IMDb rating
-	                       $genre = '',                                 // $genre Used to filter by a given genre (See http://www.imdb.com/genre/ for full list)
-                       	      $sort_by = 'date-added',                     // $sort_by Sorts the results by chosen value
+	                      $genre = '',                                 // $genre Used to filter by a given genre (See http://www.imdb.com/genre/ for full list)
+                       	  $sort_by = 'date-added',                     // $sort_by Sorts the results by chosen value
 	                      $order_by = 'desc',                          // $order_by Orders the results by either Ascending or Descending order
                        	      $with_rt_ratings = false                     // $with_rt_ratings Returns the list with the Rotten Tomatoes rating included
 	                            )
@@ -161,23 +161,37 @@ below code is used to display what you want -->
 		{
 		$title1[] = $movie->title ;                   // Movie title from api
                 $rating1[] = $movie->rating;
-                $check_geners_existance =count($movie->genres);   if ( $check_geners_existance !== 0 ) { $genres1[] = implode(",",$movie->genres); } else { $genres1[] = "Empty"; } // $genres1[] = implode(",",$movie->genres);
+                /*$genres1[] = implode(",",$movie->genres);*/$check_geners_existance =count($movie->genres);   if ( $check_geners_existance !== 0 ) { $genres1[] = implode(",",$movie->genres); } else { $genres1[] = "Empty"; }
                 $image_url[] = $movie->medium_cover_image;
+                $orignal_link_url[] = $movie->url;
                 $synopsis1[] = $movie->synopsis;
                 $imdb_code[] = $movie->imdb_code;
                 $imdb_rating[] = $movie->rating;
                 $year[] = $movie->year;
-		$language1[] = $movie->language;
-		$yt_trailer_code1[] = $movie->yt_trailer_code;
+                $language1[] = $movie->language;
+                $yt_trailer_code1[] = $movie->yt_trailer_code;
+                $torrents_counts = count($movie->torrents);
+                $torrents_count = count($torrents_counts);
+
                 /*        $url= "https://www.imdb.com/title/".$imdb_code."/mediaindex"; $ch = curl_init ($url);curl_setopt($ch, CURLOPT_HEADER, 0); curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla'); curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false); curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false); curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); curl_setopt($ch, CURLOPT_BINARYTRANSFER,1); $rawdata=curl_exec($ch); curl_close ($ch); $re = '/ <link rel=\'image_src\' href="(.*?)">/m'; preg_match_all($re, $rawdata, $matches, PREG_SET_ORDER, 0);
                 echo '<img src="'. $matches[0][1]. '" alt="Broken-image" style="width:150px;height:200px;"><br>'; */
-		$torrent[] = $movie->torrents[0];
+	        	$torrent[] = $movie->torrents[0];
                 $torrenta = $movie->torrents[0];                             // First torrent
- 		$magnet_link1[] = 'magnet:?xt=urn:btih:' . $torrenta->hash;
+                $torrenta2 = $movie->torrents[1];                             // 2nd torrent
+                $torrenta3 = $movie->torrents[2];                             // 3rd torrent
+                $torrenta4 = $movie->torrents[3];                             // 4th torrent
+ 	        	$magnet_link1[] = 'magnet:?xt=urn:btih:' . $torrenta->hash;
+ 	        	$magnet_link2[] = 'magnet:?xt=urn:btih:' . $torrenta2->hash;
+ 	        	$magnet_link3[] = 'magnet:?xt=urn:btih:' . $torrenta3->hash;
+ 	        	$magnet_link4[] = 'magnet:?xt=urn:btih:' . $torrenta4->hash;
+ 	        	
                 $size1[] = $torrenta->size;
+                $size2[] = $torrent2->size;
+                $size3[] = $torrent3->size;
+                $size4[] = $torrent4->size;
  		//echo '<a href="' . $torrent->url . '">' . $torrent->url . '</a> (' . $torrent->size . ')<br/>'; // Torrent url and size
  }
-	}
+	} echo $torrents_counts;
 ?>
  <body>
        <div id="rounded">
@@ -186,9 +200,10 @@ below code is used to display what you want -->
               <h2>Simpler is better</h2><br>
               <?php $full_url = $_SERVER['REQUEST_URI']; $qurey_url = '?'.$_SERVER['QUERY_STRING']; $full_url1 = str_replace($qurey_url,'', $full_url); ?>
               
-              <form action="<?php echo $full_url1; ?>"> <select name="quality"> <option value="all" selected="selected">All</option> <option value="720p">720p</option> <option value="1080p">1080p</option> <option value="3D">3D</option> </select> <select name="genre"> <option value="all" selected="selected">All</option> <option value="action">Action</option> <option value="adventure">Adventure</option> <option value="animation">Animation</option> <option value="biography">Biography</option> <option value="comedy">Comedy</option> <option value="crime">Crime</option> <option value="documentary">Documentary</option> <option value="drama">Drama</option> <option value="family">Family</option> <option value="fantasy">Fantasy</option> <option value="film-noir">Film-Noir</option> <option value="game-show">Game-Show</option> <option value="history">History</option> <option value="horror">Horror</option> <option value="music">Music</option> <option value="musical">Musical</option> <option value="mystery">Mystery</option> <option value="news">News</option> <option value="reality-tv">Reality-TV</option> <option value="romance">Romance</option> <option value="sci-fi">Sci-Fi</option> <option value="sport">Sport</option> <option value="talk-show">Talk-Show</option> <option value="thriller">Thriller</option> <option value="war">War</option> <option value="western">Western</option> </select> <select name="rating"> <option value="0" selected="selected">All</option> <option value="9">9+</option> <option value="8">8+</option> <option value="7">7+</option> <option value="6">6+</option> <option value="5">5+</option> <option value="4">4+</option> <option value="3">3+</option> <option value="2">2+</option> <option value="1">1+</option> </select> <select name="sort_by"> <option value="date-added" selected="selected">Date Added</option> <option value="title">Title</option> <option value="year">Year</option> <option value="rating">Ratings</option> <option value="peers">Peers</option> <option value="seeds">Seeds</option> <option value="download_count">Download Count</option> <option value="like_count">Like Count</option> </select> <input type="text" class="text" maxlength="99" name="query_term" placeholder="Search"> <input type="submit" value="Submit"> </form>
+              <form action="<?php echo $full_url1; ?>"><select name="quality"><option value="all" selected="selected"><?php if(empty($quality)) {  echo "Quality";} else  { echo $quality; } ?> </option><option value="all">All</option> <option value="720p">720p</option> <option value="1080p">1080p</option> <option value="3D">3D</option> </select> <select name="genre"> <option value="all" selected="selected"><?php if(empty($genre)) {  echo "Genre";} else  { echo $genre; } ?></option><option value="all">All</option> <option value="action">Action</option> <option value="adventure">Adventure</option> <option value="animation">Animation</option> <option value="biography">Biography</option> <option value="comedy">Comedy</option> <option value="crime">Crime</option> <option value="documentary">Documentary</option> <option value="drama">Drama</option> <option value="family">Family</option> <option value="fantasy">Fantasy</option> <option value="film-noir">Film-Noir</option> <option value="game-show">Game-Show</option> <option value="history">History</option> <option value="horror">Horror</option> <option value="music">Music</option> <option value="musical">Musical</option> <option value="mystery">Mystery</option> <option value="news">News</option> <option value="reality-tv">Reality-TV</option> <option value="romance">Romance</option> <option value="sci-fi">Sci-Fi</option> <option value="sport">Sport</option> <option value="talk-show">Talk-Show</option> <option value="thriller">Thriller</option> <option value="war">War</option> <option value="western">Western</option> </select> <select name="rating"> <option value="0" selected="selected">All</option> <option value="9">9+</option> <option value="8">8+</option> <option value="7">7+</option> <option value="6">6+</option> <option value="5">5+</option> <option value="4">4+</option> <option value="3">3+</option> <option value="2">2+</option> <option value="1">1+</option> </select> <select name="sort_by"> <option value="date-added" selected="selected">Date Added</option> <option value="title">Title</option> <option value="year">Year</option> <option value="rating">Ratings</option> <option value="peers">Peers</option> <option value="seeds">Seeds</option> <option value="download_count">Download Count</option> <option value="like_count">Like Count</option> </select> <input type="text" class="text" maxlength="99" name="query_term" placeholder="Search"> <input type="submit" value="Submit"> </form>
               <ul id="navigation">
-                   <li><a href="<?php echo $full_url1; ?>">Reload</a></li><li> <li><a href="<?php $page2 = $page1 + 1; $next_page = $full_url1.'?page='.$page2."&quality=".$quality."&genre=".$genre."&rating=".$rating."&sort_by=".$sort_by."&query_term=".$query_term; echo $next_page ?>">Next Page</a></li><li><li><a href="<?php $page3 = $page1 - 1; $prev_page = $full_url1.'?page='.$page3."&quality=".$quality."&genre=".$genre."&rating=".$rating."&sort_by=".$sort_by."&query_term=".$query_term; echo $prev_page ?>">Previous Page</a></li><li>
+                  
+                   <li><a href="<?php echo $full_url1; ?>">Reload</a></li><li> <li><a href="<?php $page2 = $page1 + 1; $next_page = $full_url1.'?page='.$page2."&quality=".$quality."&genre=".$genre."&rating=".$rating."&sort_by=".$sort_by."&query_term=".$query_term;  echo $next_page ?>">Next Page</a></li><li><li><a href="<?php $page3 = $page1 - 1; $prev_page = $full_url1.'?page='.$page3."&quality=".$quality."&genre=".$genre."&rating=".$rating."&sort_by=".$sort_by."&query_term=".$query_term; echo $prev_page ?>">Previous Page</a></li><li>
                    <?php  /* for ($x = 0; $x <= 5; $x++) { $imns = $x+1; echo '<li><a href="#'.$image_url[$x].'">Load Image '.$imns.'</a></li>'; } */ ?>
                    <li><img id="loading" src="img/ajax_load.gif" alt="loading" /></li>
               </ul>    
@@ -199,12 +214,12 @@ below code is used to display what you want -->
                           for ($y = 0; $y <= $countresults; $y++) {
                                        echo '<div id="pageContent"><b><font color="blue">'.$title1[$y].'</b></font>&emsp;('.$year[$y].')&emsp;&emsp;<b>IMDB RATING - '.$imdb_rating[$y].'</b>&emsp;<font color="brown">'.$genres1[$y].'</font>';
                                        echo '<br><b> Magnet Link = </b><a href="'.$magnet_link1[$y].'">'.$magnet_link1[$y].'</a>';
-                                       echo '<br><b> Size - </b>'.$size1[$y]; echo '&emsp;&emsp;<b> Language - </b>'.$language1[$y]; echo '&emsp;&emsp;<b> Youtube Trailer - </b><a href="https://www.youtube.com/watch?v='.$yt_trailer_code1[$y].'"> View 1</a>'; 
+                                       echo '<br><b> Size - </b>'.$size1[$y];  echo '&emsp;&emsp;<b> Language - </b>'.$language1[$y];echo '<ln><a href="#LANGUAGE'.$orignal_link_url[$y].'">check language</a></ln>'; echo '<div class="inline" id="#LANGUAGE'.$orignal_link_url[$y].'"></div>'; echo '<b> Youtube Trailer - </b><a href="https://www.youtube.com/watch?v='.$yt_trailer_code1[$y].'"> View 1</a>';
                                        echo '<br><button class="collapsiblea">Synopsis(Click to view)</button><div class="content"><p>'.$synopsis1[$y].'</p></div>';
                                        $imns = $y+1; echo '<ln><a href="#'.$image_url[$y].'"> Click to Load Image '.$imns.'</a></ln>';
                                        echo '<div id="#'.$image_url[$y].'"></div></div>';
                                                                    }
-                    ?>
+                  ?>
 
         </div>
   <div class="clear"></div> 
