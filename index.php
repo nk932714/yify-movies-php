05c37f810896b64ca9141087f63027f6ea73038b
nk <nk932714@gmail.com>
2019-02-18 15:58:18 +0530

    #15 adding some intelligence(fix)
    (fix youtube wasn't opening in popup)
    1. if you are using a mobile device and click on trailer link it will open the trailer in mobile YouTube app.
    
    2. if you are using a PC then trailer will open in popup. SO you don't need to go to YouTube to watch the trailer and come back again and again. (will save your time)
    
    3. To exit from popup you need to click anywhere outside YouTube video link.

--- index.php
+++ index.php
@@ -4,10 +4,10 @@
        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
        <title>Yify Torrents</title>
        <link rel="stylesheet" type="text/css" href="style.css" /> <!--to make language check inline --><style>div.inline { float:right; }.clearBoth { clear:both; }</style>
-       <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script> <!--youtube in site preview for pc and mobile in app preview in mob devices -->
-       <script type="text/javascript" src="youtube.js"></script> <!--youtube in site preview for pc and mobile in app preview in mob devices -->	 
        <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js"></script>
        <script type="text/javascript" src="script1.js"></script>
+       <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script> <!--youtube in site preview for pc and mobile in app preview in mob devices -->
+       <script type="text/javascript" src="youtube.js"></script> <!--youtube in site preview for pc and mobile in app preview in mob devices -->	 
  </head>
   <?php
         //get input variables from user
