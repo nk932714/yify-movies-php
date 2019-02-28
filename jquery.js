3ada10cd0742fd1ca9575bd2fc2dd69e0ca0a0a5
nk <nk932714@gmail.com>
2019-02-28 17:33:29 +0530

    #16 js for snackbar

--- jquery.js
+++ jquery.js
@@ -1,4 +1,4 @@
-/* only for click expand feature*/
+/* only for click expand feature start */
 var coll = document.getElementsByClassName("collapsiblea");
 var i;
 
@@ -13,3 +13,27 @@ for (i = 0; i < coll.length; i++) {
     } 
   });
 }
+/* click expand end */
+/* only for snackbar start*/
+function launch_toast(para) {
+  var coundownC1 = para+'C1';
+    // time counter on page js start
+      var timeleft = 3;
+    var downloadTimer = setInterval(function(){
+   // timeleft--;   document.getElementById("countdowntimer").textContent = timeleft;
+      timeleft--;   document.getElementById(coundownC1).textContent = timeleft;
+      if(timeleft === 0){ document.getElementById(coundownC1).innerHTML = "Show again"; }
+    if(timeleft <= 0)
+        clearInterval(downloadTimer);
+    },1000); 
+  //time counter on page js end
+  //time to perform curl
+  setTimeout(function(){
+    //below is the notification code js
+  var param = para+'1';
+  var x = document.getElementById(param);
+  x.classList.add("show");
+  setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
+       } , 2000);  //time remaining to display popup
+  } 
+/* snackbar end*/
